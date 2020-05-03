14f90365dfa8248e1b832caedbf2962304cb86fa
Lei <gabriel_sl19798@hotmail.com>
2020-05-04 10:37:11 +1200

    fix my x and y for moving image

---
+++ week3/js/script.js
@@ -0,0 +1,48 @@
+function showAlert() {
+    // we will use alert and console.log for debugging
+
+    // alert("hello world");
+    // console.log("Hello world in console");
+    // setTimeout is used to pause the actions
+    setTimeout(function () {
+        alert("hello world");
+        console.log("Hello world in console");
+    }, 3000);
+}
+
+function showLoop() {
+    // setInterval is used to loop animation or functions in javascript
+    // function hello() {
+    //     alert("hello world form loop");
+    //     console.log("hello world from loop");
+    // }
+    // setInterval(hello, 3000);
+
+    setInterval(function () {
+        alert("hello world from loop");
+        console.log("hello world from loop");
+    }, 3000);
+}
+
+function moveImage() {
+    image = document.getElementById("movingImg");
+    x = 10; //left
+    y = 10; //top
+    speed = Number(document.getElementById("speedInput").value);
+    // create a function in loop, and loop it each half second
+    setInterval(function () {
+        if (x == 10 && x <= 500 && y == 10){
+            x=x+speed; //only moving to right
+        }else if(y == 10 && y <= 500 && x >= 500){
+            y=y+speed; //only moving to bottom
+        }else if(x == 500 && y == 500){
+            x=x-speed; //only moving to left
+        }else if(x == 10 && y == 500){
+            y = y-speed; //only moving to top
+        }
+        console.log("x=",x);
+        console.log("y=", y);
+        image.style.left = x+"px";
+        image.style.top = y+"px";
+    }, 500);
+}
