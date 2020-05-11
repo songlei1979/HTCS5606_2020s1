a17051a03f17cfe25ac131959d6729edfe62696d
Lei <gabriel_sl19798@hotmail.com>
2020-05-11 22:00:11 +1200

    slide show

---
+++ week4/slideshow/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $("#s1").mouseenter(function () {
+        $("#bigImgs").children().fadeOut("slow");
+        $("#b1").delay( "slow" ).slideDown("slow");
+    });
+    $("#s2").mouseenter(function () {
+        $("#bigImgs").children().fadeOut("slow");
+        $("#b2").delay( "slow" ).slideDown("slow");
+    });
+    $("#s3").mouseenter(function () {
+        $("#bigImgs").children().fadeOut("slow");
+        $("#b3").delay( "slow" ).slideDown("slow");
+    });
+    $("#s4").mouseenter(function () {
+        $("#bigImgs").children().fadeOut("slow");
+        $("#b4").delay( "slow" ).slideDown("slow");
+    });
+    $("#s5").mouseenter(function () {
+        $("#bigImgs").children().fadeOut("slow");
+        $("#b5").delay( "slow" ).slideDown("slow");
+    });
+});
