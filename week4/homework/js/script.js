fccae4cd208bb86f183c36fd2efcc571f63e74ad
Lei <gabriel_sl19798@hotmail.com>
2020-05-14 10:15:48 +1200

    api exercise

---
+++ week4/homework/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: 'https://djangorestframestudy.herokuapp.com/api/leads/',
+        dataType: "JSON", // data type expected from server
+        success: function (data) {
+            i = 0; //set counter i
+            while (i < data.length){ //data from our console, it is an array, I don't the size of that array, I use length
+                person = data[i]; //each item in array (data), it is a person record
+                $("#records").append("<tr>" +
+                    "<td>"+person.id+"</td>" +
+                    "<td>"+person.name+"</td>" +
+                    "<td>"+person.email+"</td>" +
+                    "<td>"+person.message+"</td>" +
+                    "<td><button class='showPerson' personID='"+person.id+"'>Show</button></td>" +
+                    "</tr>");
+                i = i+1;
+            }
+            $(".showPerson").click(function () {
+                personID = $(this).attr("personID"); // this is the clicked button, we take value of person ID on this button
+                $.ajax({
+                    type: 'GET',
+                    url:'https://djangorestframestudy.herokuapp.com/api/leads/'+personID+'/',
+                    success: function (data) {
+                        console.log(data);
+                        $("#personInfo").empty(); //clean my divider
+                        $("#personInfo").append("" +
+                            "<p>ID: "+data.id+"</p>" +
+                            "<p>Name: "+data.name+"</p>" +
+                            "<p>Email: "+data.email+"</p>" +
+                            "<p>Message: "+data.message+"</p>" +
+                            "");
+                    },
+                    error: function () {
+                        alert("error");
+                    }
+                })
+            });
+        },
+        error: function() {
+            console.log('Error: ' + data);
+
+        }
+    });
+});
--- week4/js/weatherScript.js
+++ week4/js/weatherScript.js
@@ -1,23 +1,29 @@
 $(document).ready(function () {
 
-    $.ajax({
-        type: 'GET', //action get, post, put, delete
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=Auckland&appid=cdf05d929ed5cfa526764c43d2b832d2',
-        dataType: "JSON", // data type expected from server
-        success: function (data) {
-            console.log(data);
-            $("#locationPlace").text(data.name);
-            $("#forecastPlace").text(data.weather[0].description);
-            $("#tempPlace").text(data.main.temp);
-            $("#windPlace").text(data.wind.speed);
-            $("#icon").html("<img src='http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>");
+    $("#submitBtn").click(function () {
+        city = $("#cityName").val();
+        $.ajax({
+            type: 'GET', //action get, post, put, delete
+            // Auckland was a word inside url, we don't have to change
+            // city is a variable, if we want to add a variable to a string, we need + it in
+            url: 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cdf05d929ed5cfa526764c43d2b832d2',
+            dataType: "JSON", // data type expected from server
+            success: function (data) {
+                console.log(data);
+                $("#locationPlace").text(data.name);
+                $("#forecastPlace").text(data.weather[0].description);
+                $("#tempPlace").text(data.main.temp);
+                $("#windPlace").text(data.wind.speed);
+                $("#icon").html("<img src='http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'>");
 
-        },
-        error: function() {
-            console.log('Error: ' + data);
+            },
+            error: function() {
+                console.log('Error: ' + data);
 
-        }
+            }
+        });
     });
+
 
 });
 
