deba0218c1d14b815150945f0e5d11e66a47b1c2
Lei <gabriel_sl19798@hotmail.com>
2020-05-18 16:37:04 +1200

    api post and delete solution

--- week4/homework/js/script.js
+++ week4/homework/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    getPeople();
+
+    $("#addBtn").click(function () {
+        name1 = $("#name").val();
+        email = $("#email").val();
+        msg = $("#msg").val();
+        addPersion(name1, email, msg);
+    })
+});
+
+function getPeople() {
     $.ajax({
         type: 'GET', //post
         url: 'https://djangorestframestudy.herokuapp.com/api/leads/',
@@ -12,29 +23,21 @@ $(document).ready(function () {
                     "<td>"+person.name+"</td>" +
                     "<td>"+person.email+"</td>" +
                     "<td>"+person.message+"</td>" +
-                    "<td><button class='showPerson' personID='"+person.id+"'>Show</button></td>" +
+                    "<td>" +
+                    "<button class='showPerson' personID='"+person.id+"'>Show</button>" +
+                    "<button class='deletePerson' personID='"+person.id+"'>Delete</button>" +
+                    "</td>" +
                     "</tr>");
                 i = i+1;
             }
             $(".showPerson").click(function () {
                 personID = $(this).attr("personID"); // this is the clicked button, we take value of person ID on this button
-                $.ajax({
-                    type: 'GET', //delete
-                    url:'https://djangorestframestudy.herokuapp.com/api/leads/'+personID+'/',
-                    success: function (data) {
-                        console.log(data);
-                        $("#personInfo").empty(); //clean my divider
-                        $("#personInfo").append("" +
-                            "<p>ID: "+data.id+"</p>" +
-                            "<p>Name: "+data.name+"</p>" +
-                            "<p>Email: "+data.email+"</p>" +
-                            "<p>Message: "+data.message+"</p>" +
-                            "");
-                    },
-                    error: function () {
-                        alert("error");
-                    }
-                })
+                getPersionInfo(personID);
+            });
+            $(".deletePerson").click(function () {
+                personID = $(this).attr("personID"); // this is the clicked button, we take value of person ID on this button
+                deletePersion(personID);
+                $(this).parent().parent().remove();
             });
         },
         error: function() {
@@ -42,4 +45,57 @@ $(document).ready(function () {
 
         }
     });
-});
+}
+
+function getPersionInfo(personID) {
+    $.ajax({
+        type: 'GET', //delete
+        url:'https://djangorestframestudy.herokuapp.com/api/leads/'+personID+'/',
+        success: function (data) {
+            console.log(data);
+            $("#personInfo").empty(); //clean my divider
+            $("#personInfo").append("" +
+                "<p>ID: "+data.id+"</p>" +
+                "<p>Name: "+data.name+"</p>" +
+                "<p>Email: "+data.email+"</p>" +
+                "<p>Message: "+data.message+"</p>" +
+                "");
+        },
+        error: function () {
+            alert("error");
+        }
+    })
+}
+
+function deletePersion(personID) {
+    $.ajax({
+        type: 'DELETE', //delete
+        url:'https://djangorestframestudy.herokuapp.com/api/leads/'+personID+'/',
+        success: function (data) {
+            $("#personInfo").empty();
+            alert("Person Deleted");
+        },
+        error: function () {
+            alert("error");
+        }
+    })
+}
+
+function addPersion(name, email, msg) {
+    $.ajax({
+        type: "POST",
+        url: "https://djangorestframestudy.herokuapp.com/api/leads/",
+        dataType: "JSON",
+        data: {
+            "name":name,
+            "email":email,
+            "message": msg
+        },
+        success: function () {
+            alert("person added");
+        },
+        error: function(){
+            alert("add failed");
+        }
+    });
+}
